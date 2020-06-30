2e909b93c5c5c7154ec1060c8fb281495def1fd4
Travis F. Collins <travis.collins@analog.com>
2020-06-30 10:02:49 -0700

    Update getDockerConfig.groovy

--- vars/getDockerConfig.groovy
+++ vars/getDockerConfig.groovy
@@ -8,16 +8,17 @@ def call(java.util.ArrayList listOfResources){
   for (i = 0; i<listOfResources.size(); i++) {
     echo "${listOfResources[i]}"
 
-    if (listOfResources[i] == 'MATLAB') {
+    if (listOfResources[i].equalsIgnoreCase( 'MATLAB' )) {
         args.add('-v "/nfs/apps/MATLAB":"/usr/local/MATLAB":ro')
         args.add('-v "/nfs/apps/resources/dot_matlab":"/root/.matlab":ro')
         args.add('-v "/nfs/apps/resources/mlhsp":"/mlhsp":ro')
     }
-    if (listOfResources[i] == 'Vivado') {
+    if (listOfResources[i].equalsIgnoreCase( 'Vivado' )) {
         args.add('-v "/nfs/apps/Xilinx":"/opt/Xilinx":ro')
         args.add('-v "/nfs/apps/resources/dot_Xilinx":"/root/.Xilinx":ro')
     }
 
   }
 
+  return args
 }
