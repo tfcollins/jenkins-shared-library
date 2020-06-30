96f0c24df6dad94d0f64532a68041fa8cde533bb
Travis F. Collins <travis.collins@analog.com>
2020-06-30 10:38:02 -0700

    Update dockerParallelBuild.groovy

--- vars/dockerParallelBuild.groovy
+++ vars/dockerParallelBuild.groovy
@@ -28,7 +28,7 @@ buildParallelWithDocker(branches2, dockerArgs2){
 }
 */
 
-def call(branchNames, dockerHost, dockerArgs, dockerstages(branchName)){
+def call(branchNames, dockerHost, dockerArgs, dockerstages){
 
   def tests = [:]
   //def dockerHost = 'ubuntu-vm'
