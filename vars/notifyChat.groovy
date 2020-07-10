e35e2e7f4746df7866eff7adbb54212c07bd6d56
Travis F. Collins <travis.collins@analog.com>
2020-07-10 16:41:12 -0700

    Create notifyChat.groovy

---
+++ vars/notifyChat.groovy
@@ -0,0 +1,15 @@
+def call(project, message) {
+
+  if (project=='TransceiverToolbox') {
+    channel = '#matlab'
+  }
+  else if (project=='hdl') {
+    channel = '#hdl'
+  }
+  else {
+    channel = '#random'
+  }
+
+  slackSend(channel: channel, message: message)
+
+}
