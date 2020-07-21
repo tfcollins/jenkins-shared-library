3a173771cb94040252b70752e4b81f239608e70d
Travis F. Collins <travis.collins@analog.com>
2020-07-20 20:33:38 -0700

    Update uploadFTP.groovy

--- vars/uploadFTP.groovy
+++ vars/uploadFTP.groovy
@@ -60,7 +60,7 @@ def call(project, filename) {
       upload_target = FTP_ROOT_TARGET+target
       println("Uploading: $filename")
       println("Target: $upload_target")
-      //commands = "set ssl:verify-certificate no; set ftp:ssl-allow no; cd "+upload_target+"; ls; bye"
+    
       commands = "set ssl:verify-certificate no; set ftp:ssl-allow no; cd "+upload_target+"; mput "+filename+"; bye"
       sh 'lftp -e "'+commands+'" -u $FTP_USERNAME,$FTP_PASS $FTP_SERVER'
       
