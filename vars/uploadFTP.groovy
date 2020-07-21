5186e02a97ce8c6fcff33954392cab42f3141cc5
Travis F. Collins <travis.collins@analog.com>
2020-07-20 17:53:16 -0700

    Update uploadFTP.groovy

--- vars/uploadFTP.groovy
+++ vars/uploadFTP.groovy
@@ -40,7 +40,7 @@ def call(project, filename) {
   //  TransceiverToolbox/release/trx-toolbox-tag/<files>
   
   // Check if we have files to upload based on target
-  sh 'ls '+filepattern+' > files_searched || true'
+  sh 'ls '+filename+' > files_searched || true'
   def files_list = readFile('files_searched')
   println("Files found: "+files_list)
   if (files_list.length() <= 0) {
