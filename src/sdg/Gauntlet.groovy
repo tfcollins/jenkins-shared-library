529c09aa01228ef4f3c1d87baad1966c34581961
Travis F. Collins <travis.collins@analog.com>
2020-08-19 19:41:43 -0700

    Update Gauntlet.groovy

--- src/sdg/Gauntlet.groovy
+++ src/sdg/Gauntlet.groovy
@@ -108,7 +108,9 @@ def stage_library(String stage_name){
                 run_i("pip3 install -r requirements.txt")
                 run_i("pip3 install -r requirements_dev.txt")
                 run_i("pip3 install pylibiio")
-                run_i("python3 -m pytest -v -k 'not stress' -s --uri='ip:"+ip+"' -m "+board.replaceAll("-","_"))
+                run_i("mkdir testxml")
+                run_i("python3 -m pytest --junitxml=testxml/ -v -k 'not stress' -s --uri='ip:"+ip+"' -m "+board.replaceAll("-","_"))
+                junit testResults: 'test/*.xml', allowEmptyResults: true
             }
           }
         }
