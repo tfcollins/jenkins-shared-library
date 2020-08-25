ac6815437c00b1e609c981b62aae98788ee72758
Travis F. Collins <travis.collins@analog.com>
2020-08-24 18:14:15 -0700

    Update Gauntlet.groovy

--- src/sdg/Gauntlet.groovy
+++ src/sdg/Gauntlet.groovy
@@ -99,11 +99,16 @@ def stage_library(String stage_name){
     case "LinuxTests":
       println("Added Stage LinuxTests")
       cls = {
-        stage("Linux Tests") {
-          run_i("pip3 install pylibiio")
-          ip = nebula("uart.get-ip")
-          nebula("net.check-dmesg --ip='"+ip+"'")
-          nebula('driver.check-iio-devices --uri="ip:'+ip+'"')
+        try {
+         stage("Linux Tests") {
+           run_i("pip3 install pylibiio")
+           ip = nebula("uart.get-ip")
+           nebula("net.check-dmesg --ip='"+ip+"'")
+           nebula('driver.check-iio-devices --uri="ip:'+ip+'"')
+         }
+        }
+        finally {
+         archiveArtifacts artifacts: '*.log', followSymlinks: false, allowEmptyArchive: true
         }
       };
       break;
