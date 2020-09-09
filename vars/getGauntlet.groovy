5a839ce02e8edff3385481263e269812f87851ab
Travis F. Collins <travis.collins@analog.com>
2020-09-09 11:14:25 -0700

    Update getGauntlet.groovy

--- vars/getGauntlet.groovy
+++ vars/getGauntlet.groovy
@@ -1,5 +1,5 @@
-def call(depends, hdlBranch, linuxBranch, bootfile_source) {
+def call(depends, hdlBranch, linuxBranch, firmwareVersion, bootfile_source) {
     def harness =  new sdg.Gauntlet()
-    harness.construct(depends, hdlBranch, linuxBranch, bootfile_source)
+    harness.construct(depends, hdlBranch, linuxBranch, firmwareVersion, bootfile_source)
     return harness
 }
