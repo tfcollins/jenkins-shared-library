e12fcbed1a0a372c9744ac336cf8dd71236a95c7
Paller <KimChesed.Paller@analog.com>
2021-01-26 10:55:45 +0800

    Gauntlet: stage_library: Add LibAD9361Tests

--- vars/getGauntlet.groovy
+++ vars/getGauntlet.groovy
@@ -1,5 +1,5 @@
-def call(depends, hdlBranch, linuxBranch, firmwareVersion, bootfile_source) {
+def call(depends, hdlBranch, linuxBranch, firmwareVersion, bootfile_source, libad9361Version=null) {
     def harness =  new sdg.Gauntlet()
-    harness.construct(depends, hdlBranch, linuxBranch, firmwareVersion, bootfile_source)
+    harness.construct(depends, hdlBranch, linuxBranch, firmwareVersion, bootfile_source, libad9361Version)
     return harness
 }
