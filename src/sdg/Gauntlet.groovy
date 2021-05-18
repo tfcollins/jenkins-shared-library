9a65db3e9975df50caa80c7c49b7f2c28032352c
Travis F. Collins <travis.collins@analog.com>
2021-05-17 23:31:17 -0700

    Make docker host mode configureable (#15)
    * Make docker hostmode configurable
    
    * Update Gauntlet.groovy

--- src/sdg/Gauntlet.groovy
+++ src/sdg/Gauntlet.groovy
@@ -34,6 +34,7 @@ def construct(List dependencies, hdlBranch, linuxBranch, bootPartitionBranch, fi
             enable_docker: false,
             docker_image: 'tfcollins/sw-ci:latest',
             docker_args: ['MATLAB','Vivado'],
+            docker_host_mode: true,
             enable_update_boot_pre_docker: false,
             setup_called: false,
             nebula_debug: false,
@@ -361,7 +362,9 @@ private def run_agents() {
     docker_args.add('-v /etc/default:/default:ro')
     docker_args.add('-v /dev:/dev')
     docker_args.add('-v /usr/app:/app')
-    docker_args.add('--network host')
+    if (gauntEnv.docker_host_mode) {
+        docker_args.add('--network host')
+    }
     if (docker_args instanceof List) {
         docker_args = docker_args.join(' ')
     }
@@ -494,6 +497,14 @@ def set_docker_args(docker_args) {
  */
 def set_enable_docker(enable_docker) {
     gauntEnv.enable_docker = enable_docker
+}
+
+/**
+ * Enable use of docker host mode.
+ * @param docker_host_mode boolean True will enable use of docker host mode
+ */
+def set_docker_host_mode(docker_host_mode) {
+    gauntEnv.docker_host_mode = docker_host_mode
 }
 
 /**
