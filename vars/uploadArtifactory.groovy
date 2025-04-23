0e42bc794d39d2345957c92d307547382d4e8153
Travis F. Collins <travis.collins@analog.com>
2025-04-23 15:14:10 -0600

    Update uploadArtifactory.groovy

--- vars/uploadArtifactory.groovy
+++ vars/uploadArtifactory.groovy
@@ -153,6 +153,27 @@ def call(project, filepattern, subproject='') {
         //         target = target + 'dev/' + branch
         // }
   }
+  else if (project == 'ApolloConfigurator') {
+        ext = '.mltbx'
+        name = 'ad9084_configurator'
+        target = root + 'ad9084_configurator/'
+
+        def branch = env.BRANCH_NAME
+        if (!env.BRANCH_NAME) {
+            println('Branch name not found in environment, checking through git')
+            sh 'git branch > branchname'
+            sh 'sed -i "s/[*]//" branchname'
+            branch = readFile('branchname').trim()
+        }
+
+        println('Found branch: ' + branch)
+        if (branch == 'main') {
+            target = target + 'main'
+        }
+    else {
+            target = target + 'dev/' + branch
+    }
+  }
   else {
         println('Unknown project. Not uploading artifacts')
         return
